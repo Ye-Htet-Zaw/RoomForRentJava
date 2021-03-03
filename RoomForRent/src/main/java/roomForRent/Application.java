ab69db497bc664f7b3a8beed8eaae0147b201f11
Hsu Nandar Tun <hsunandartun6@gmail.com>
2021-03-03 14:00:55 +0630

    # WARNING: head commit changed in the meantime
    first commit

--- RoomForRent/src/main/java/roomForRent/Application.java
+++ RoomForRent/src/main/java/roomForRent/Application.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		//this is my changes
-		//this is my second changes
-		//this is my third changes
+		
 	}
 }
