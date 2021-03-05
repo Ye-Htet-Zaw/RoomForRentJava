1f5c4fcf6621e3a08a988989058b3f104285f430
nttt111 <nantinttinttun.gic@gmail.com>
2021-03-05 12:04:40 +0630

    //login

---
+++ RoomForRent/src/main/java/roomForRent/dto/owner/LoginDto.java
@@ -0,0 +1,18 @@
+package roomForRent.dto.owner;
+
+public class LoginDto {
+	String user_email;
+	String password;
+	public String getUser_email() {
+		return user_email;
+	}
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
--- RoomForRent/src/main/java/roomForRent/service/owner/UserService.java
+++ RoomForRent/src/main/java/roomForRent/service/owner/UserService.java
@@ -1,5 +1,22 @@
 package roomForRent.service.owner;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import roomForRent.dto.owner.LoginDto;
+import roomForRent.dto.owner.UserDto;
+import roomForRent.mapper.owner.UserMapper;
+
+@Service
 public class UserService {
+	
+	
+	@Autowired
+	UserMapper userMapper;
+	
+	public UserDto getUserWithEmailAndPassword(LoginDto loginDto) {
+		
+		return userMapper.getUserWithEmailAndPassword(loginDto);
+	}
 
 }
