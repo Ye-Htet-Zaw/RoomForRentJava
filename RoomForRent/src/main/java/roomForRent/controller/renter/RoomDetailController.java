29551953466e8f55e7aa10982a4bb739d3ca7e5d
Ye-Htet-Zaw <yehtetzaw.gic@gmail.com>
2021-03-05 09:53:32 +0630

    YHZ

--- RoomForRent/src/main/java/roomForRent/controller/renter/RoomDetailController.java
+++ RoomForRent/src/main/java/roomForRent/controller/renter/RoomDetailController.java
@@ -1,5 +1,22 @@
 package roomForRent.controller.renter;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import roomForRent.dto.renter.RoomDetailDto;
+import roomForRent.service.renter.RoomDetailService;
+
+@RestController
 public class RoomDetailController {
 
+	@Autowired
+	RoomDetailService roomDetailService;
+	
+	@GetMapping("/roomDetail/{id}")
+	public RoomDetailDto getRoomDetailById(@PathVariable(value = "id") String houseId) {	
+		RoomDetailDto roomDetailById = roomDetailService.getRoomDetailById(houseId);	
+		return roomDetailById;
+	}
 }
--- RoomForRent/src/main/java/roomForRent/service/renter/RoomDetailService.java
+++ RoomForRent/src/main/java/roomForRent/service/renter/RoomDetailService.java
@@ -1,5 +1,18 @@
 package roomForRent.service.renter;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import roomForRent.dto.renter.RoomDetailDto;
+import roomForRent.mapper.renter.RoomDetailMapper;
+
+@Service
 public class RoomDetailService {
+
+	@Autowired
+	RoomDetailMapper roomDetailMapper;
+	public RoomDetailDto getRoomDetailById(String houseId) {
+		return roomDetailMapper.getRoomDetailById(houseId);
+	}
 
 }
