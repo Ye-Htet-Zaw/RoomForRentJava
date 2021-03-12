a41e128d6b6bcb6286b169108f967bc0dd46610a
Hsu Nandar Tun <hsunandartun6@gmail.com>
2021-03-12 15:15:34 +0630

    HNT(Image Add)

--- RoomForRent/src/main/java/roomForRent/controller/renter/HouseListController.java
+++ RoomForRent/src/main/java/roomForRent/controller/renter/HouseListController.java
@@ -8,6 +8,7 @@
 */
 package roomForRent.controller.renter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,7 +31,15 @@ public class HouseListController {
 	@GetMapping("/houseLists")
 	public List<HouseListDto> getHouseList() {	
 		List<HouseListDto> houseList = houseListService.getHouseList();
-		return houseList;
+		for (int i = 0; i <houseList.size(); i++) {
+            String id = houseList.get(i).getHouse_id();
+            //accList.get(i).setImage("http://192.168.100.4:9090/images/"+id+".jpg");
+            ArrayList<String> images = new ArrayList<>();
+            for(int j = 1; j<=10; j++) {
+            	images.add("http://192.168.43.148:9090/images/house/"+id+"/"+j+".jpg");
+            }
 		
+		}
+		return houseList;
 	}
 }
