6db987514cce0636d85a4ebd87ddf7425238c903
Hsu Nandar Tun <hsunandartun6@gmail.com>
2021-05-05 14:19:16 +0630

    HNT(insert image)

--- RoomForRent/src/main/java/roomForRent/controller/owner/PostHouseController.java
+++ RoomForRent/src/main/java/roomForRent/controller/owner/PostHouseController.java
@@ -1,9 +1,13 @@
 package roomForRent.controller.owner;
 
+import java.io.File;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import roomForRent.dto.renter.HouseDto;
 import roomForRent.service.owner.PostHouseService;
@@ -18,5 +22,26 @@ public class PostHouseController {
 	public void createHouse(@RequestBody HouseDto houseDto) {
 		postHouseService.createHouse(houseDto);
 	}
+	
+	@PostMapping("/uploadImages")
+    public void uploadFile(@RequestParam("file") MultipartFile file, String path) {
+		
+		postHouseService.storeFile(file,path);
+
+
+    }
+	
+	@PostMapping("/uploadMultipleFiles")
+	   public void uploadMultipleFiles(@RequestParam("imageupload") MultipartFile[] files) {
+		  String path = "src\\main\\resources\\static\\image\\house\\";
+		  File ss = new File(path+"house");
+		  String filePath = null;
+		  if(ss.mkdir()) {
+			  filePath="src\\main\\resources\\static\\image\\house\\"+ss.getName();
+		  }
+	         for (MultipartFile multipartFile : files) {
+				uploadFile(multipartFile,filePath);
+			}
+	 }
 
 }
