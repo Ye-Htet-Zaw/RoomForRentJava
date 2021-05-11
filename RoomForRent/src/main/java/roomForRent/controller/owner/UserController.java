5a97afc340670b626ce9f0077c3afb6dbda1a7b3
Ye-Htet-Zaw <yehtetzaw.gic@gmail.com>
2021-05-11 16:37:28 +0630

    YHZ

--- RoomForRent/src/main/java/roomForRent/controller/owner/UserController.java
+++ RoomForRent/src/main/java/roomForRent/controller/owner/UserController.java
@@ -1,3 +1,10 @@
+/**
+*UserController
+*
+* 2021/03/05 CSH Create New
+*
+* For Owner Profile
+*/
 package roomForRent.controller.owner;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,27 +13,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-
 import roomForRent.dto.owner.UserDto;
+import roomForRent.service.owner.PostHouseService;
 import roomForRent.service.owner.UserService;
 
-
-/**
-*UserController
-*
-* 2021/03/05 CSH Create New
-*
-* For Owner Profile
-*/
 @RestController
 public class UserController {
 
 	@Autowired
 	UserService userService;
 
+	@Autowired
+	PostHouseService postHouseService;
 	
+	String userId;
 	
 	@GetMapping("/getUserInfo/{user_id}")
 	public UserDto getUserInfo(@PathVariable(value = "user_id")String user_id) {
@@ -36,9 +40,26 @@ public class UserController {
 	
 	@PutMapping("/updateUserInfo")
 	public void updateUserInfo(@RequestBody UserDto userDto) {
-		 userService.updateUserInfo(userDto);
+		userId = userDto.getUser_id();
+		System.out.println(userId);
+		userService.updateUserInfo(userDto);
 		
 	}
+	
+	@PostMapping("/uploadUserImages")
+    public void uploadFile(@RequestParam("file") MultipartFile file, String path,String imagename) {
+	
+		userService.storeFile(file,path,imagename);
+    }
+		
+	@PostMapping("/uploadUserMultipleFiles")
+	 public void uploadMultipleFiles(@RequestParam("imageupload") MultipartFile[] files) {
+		  String path = "src\\main\\resources\\static\\image\\user\\";
+		  System.out.println(userId);
+	      for (MultipartFile multipartFile : files) {
+	    	  uploadFile(multipartFile,path,userId);
+	      }
+	 }
 	
 	 public void addResourceHandlers(ResourceHandlerRegistry registry) {
 	     registry.addResourceHandler("/image/user**")
--- RoomForRent/src/main/java/roomForRent/service/owner/UserService.java
+++ RoomForRent/src/main/java/roomForRent/service/owner/UserService.java
@@ -1,13 +1,3 @@
-package roomForRent.service.owner;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import roomForRent.dto.owner.UserDto;
-import roomForRent.mapper.owner.UserMapper;
 /**
 *UserService
 *
@@ -15,11 +5,30 @@ import roomForRent.mapper.owner.UserMapper;
 *
 * For owner profile
 */
+package roomForRent.service.owner;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import roomForRent.dto.owner.UserDto;
+import roomForRent.mapper.owner.UserMapper;
+
 @Service
 public class UserService {
 	
 	@Autowired
 	UserMapper userMapper;
+	
+	private Path fileStorageLocation;
 	
 
 	public UserDto getUserInfo(String user_id) {
@@ -44,5 +53,30 @@ public class UserService {
 		userDto.setUpdate_datetime(new Date());
 		 userMapper.updateUserInfo(userDto);
 	}
+	
+	public void storeFile(MultipartFile file,String path,String imagename) {
+    	
+	   	 this.fileStorageLocation = Paths.get(path)
+	   			 .toAbsolutePath()
+	                .normalize();
+	   	 
+	   	 try {
+				Files.createDirectories(this.fileStorageLocation);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	   	String imName=imagename.toString()+".jpg";
+	       // Normalize file name
+	       String fileName = StringUtils.cleanPath(imName);
 
+	       try {
+	           // Copy file to the target location (Replacing existing file with the same name)
+	           Path targetLocation =  this.fileStorageLocation.resolve(fileName);
+	           //Files.delete(targetLocation);
+	           Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);	           
+	       } catch (IOException ex) {
+	           
+	       }
+			//return fileName;
+	   }
 }
