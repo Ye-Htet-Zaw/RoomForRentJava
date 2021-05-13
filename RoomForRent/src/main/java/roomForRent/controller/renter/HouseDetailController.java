c0d53cabdf9298d7b9aeebf57ebda693949355af
Ye-Htet-Zaw <79823747+Ye-Htet-Zaw@users.noreply.github.com>
2021-05-13 17:57:33 +0630

    YHZ

--- RoomForRent/src/main/java/roomForRent/controller/renter/HouseDetailController.java
+++ RoomForRent/src/main/java/roomForRent/controller/renter/HouseDetailController.java
@@ -8,9 +8,14 @@
 */
 package roomForRent.controller.renter;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,14 +28,43 @@ public class HouseDetailController {
 	@Autowired
 	HouseDetailService houseDetailService;
 	
+	private Path fileStorageLocation;
+	
 	@GetMapping("/houseDetail/{id}")
 	public HouseDetailDto getHouseDetailById(@PathVariable(value = "id") String houseId) {	
 		HouseDetailDto houseDetailById = houseDetailService.getHouseDetailById(houseId);	
 		List<String> test = new ArrayList<>();
+		String path = "src\\main\\resources\\static\\image\\house\\"+houseId;
 		for(int j= 1; j<=10; j++) {	
-			test.add("http://192.168.0.107:8080/image/house/"+houseId+"/"+j+".jpg");		
+			boolean imageExists = storeFile(path, j);
+			if (imageExists == true) {
+				test.add("http://192.168.0.107:8080/image/house/"+houseId+"/"+j+".jpg");
+				System.out.println(test);
+			};
 		}
 		houseDetailById.setHouse_image(test);
 		return houseDetailById;
 	}
+	
+	private Boolean storeFile(String path,Integer imagename) {
+    	
+	   	 this.fileStorageLocation = Paths.get(path)
+	   			 .toAbsolutePath()
+	                .normalize();
+	   	 
+	   	 	try {
+	   	 			Files.createDirectories(this.fileStorageLocation);
+	   	 		} catch (IOException e) {
+	   	 			e.printStackTrace();
+	   	 		}
+	   	 	String imName=imagename.toString()+".jpg";
+	   	 	// Normalize file name
+	   	 	String fileName = StringUtils.cleanPath(imName);
+
+	        Path targetLocation =  this.fileStorageLocation.resolve(fileName);
+	         
+	        boolean imageExists = Files.isExecutable(targetLocation);
+	   
+			return imageExists;
+	   }
 }
